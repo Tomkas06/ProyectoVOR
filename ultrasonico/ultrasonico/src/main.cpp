94c55ac1ac45d084a3682c51926e351bc9361e4f
Tomkas <76975378+Tomkas06@users.noreply.github.com>
2024-08-01 17:27:03 -0300

    Modificacion del sonar ultrasonico
    El programa detalla cuanto tiempo tarda en hacer un giro completo

--- ultrasonico/ultrasonico/src/main.cpp
+++ ultrasonico/ultrasonico/src/main.cpp
@@ -1,7 +1,7 @@
 #include <NewPing.h>
 
-#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN  16  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     17  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 180 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
@@ -11,8 +11,13 @@ void setup() {
 }
 
 void loop() {
-  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
+  for(int i = 0; i<=360; i++){
+    if (sonar.ping_cm() == 0){
+      Serial.println("--Fuera de rango--");
+    }else{
+      Serial.println(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+    }
+    delay(30);  
+  }
+  Serial.println(millis());
 }
