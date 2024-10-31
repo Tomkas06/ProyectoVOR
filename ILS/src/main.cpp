a96b9663b405439d1cd499bc064292f12083ba83
Tomkas <76975378+Tomkas06@users.noreply.github.com>
2024-10-31 12:20:30 -0300

    ILS inalámbrico listo
    Se termino los ultimos detalles al ILS. Solo queda probarlo.
    De aqui en adelante, los proximos cambios seran leves para configurar las placas

--- ILS/src/main.cpp
+++ ILS/src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+
+#include <esp_now.h>
+#include <WiFi.h>
+
 #include <NewPing.h>
 
 #define TRIGGER_PIN1 4  // Arduino pin tied to trigger pin on the ultrasonic sensor.
@@ -27,16 +31,52 @@ NewPing sonarDn(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCEDN); // NewPing setup of pi
 
 NewPing sonarLh(TRIGGER_PIN4, ECHO_PIN4, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
+uint8_t broadcastAddress[] = {0xE0, 0x98, 0x06, 0x99, 0xA2, 0x1C};
+
+typedef struct struct_message {
+    int id;
+    int x;
+    int y;
+    int z;
+    int w;
+} struct_message;
+
+struct_message myData;
+esp_now_peer_info_t peerInfo;
+
 bool isDetecUp = false;
 bool isDetecRh = false;
 bool isDetecDn = false;
 bool isDetecLh = false;
 
-String enviar[4];
-String result = "";
+int up = 1;
+int rh = 0;
+int dn = 0;
+int lh = 0;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 void setup() {
   Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
 void loop() {
@@ -49,28 +89,35 @@ void loop() {
 
 
   
-  if (isDetecUp){enviar[0] = "0"; isDetecUp = false;}
-  else{enviar[0] = "1";}
+  if (isDetecUp){up = 0; isDetecUp = false;}
+  else{up = 1;}
 
-  if (isDetecRh){enviar[1] = "1"; isDetecRh = false;}
-  else{enviar[1] = "0";}
+  if (isDetecRh){rh = 1; isDetecRh = false;}
+  else{rh = 0;}
 
-  if (isDetecDn){enviar[2] = "1"; isDetecDn = false;}
-  else{enviar[2] = "0";}
+  if (isDetecDn){dn = 1; isDetecDn = false;}
+  else{dn = 0;}
 
-  if (isDetecLh){enviar[3] = "1"; isDetecLh = false;}
-  else{enviar[3] = "0";}
+  if (isDetecLh){lh = 1; isDetecLh = false;}
+  else{lh = 0;}
   
 
-  
-  for (int i = 0; i < 4; i++) {
-    result += enviar[i]; 
-    if(i != 3){result += ",";}
+  myData.id = 1;
+  myData.x = up;
+  myData.y = rh;
+  myData.z = dn;
+  myData.w = lh;
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
   }
-  Serial.print("180,100,180");
-  Serial.print(",");
-  Serial.println(result);
-  result ="";
+  else {
+    Serial.println("Error sending the data");
+  }
+  
+  
   delay(100);
   
 }
