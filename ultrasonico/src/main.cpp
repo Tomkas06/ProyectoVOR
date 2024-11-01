27d0cdbb7046abc12059abc7811f255197d2452f
Tomkas <76975378+Tomkas06@users.noreply.github.com>
2024-11-01 11:47:26 -0300

    no se que estoy haciendo

--- ultrasonico/src/main.cpp
+++ ultrasonico/src/main.cpp
@@ -3,7 +3,7 @@
 #define TRIGGER_PIN1  14  // D5
 #define ECHO_PIN1     12  // D6
 
-#define MAX_DISTANCE 100 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 20 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
